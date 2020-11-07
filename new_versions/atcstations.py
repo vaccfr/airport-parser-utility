fe40a0009281b28b4bddb7d4a621a8728a919833
FrenchGap <peter.pare.new@gmail.com>
2020-11-07 09:26:42 +0100

    New stuff & new positions & new login profiles

---
+++ new_versions/atcstations.py
@@ -0,0 +1,118 @@
+import json
+
+"""
+[
+  {
+    "id":
+    "icao":
+    "positions: [
+      {
+        "code":
+        "frequency":
+        "callsign":
+        "rank":
+        "solo_rank":
+      }
+    ]
+    "atis_frequency":
+  }
+]
+"""
+
+position_ranks = {
+  "DEL": {
+    "full": "S1",
+    "solo": "S1",
+  },
+  "GND":  {
+    "full": "S1",
+    "solo": "S1",
+  },
+  "TWR":  {
+    "full": "S2",
+    "solo": "S1",
+  },
+  "APP":  {
+    "full": "S3",
+    "solo": "S2",
+  },
+  "DEP":  {
+    "full": "S3",
+    "solo": "S2",
+  },
+  "CTR":  {
+    "full": "C1",
+    "solo": "S3",
+  },
+  "FSS":  {
+    "full": "C1",
+    "solo": "C1",
+  },
+}
+
+pfile = "VoiceChannels.txt"
+
+def data_reader(datafile):
+  with open(datafile) as pfile_dec:
+    pfile_data = pfile_dec.readlines()
+    pfile_dec.close()
+  
+  return pfile_data
+
+def data_builder(datafile):
+  output = []
+
+  positions = data_reader(datafile)
+
+  airports = []
+  idx = 1
+  for p in positions:
+    airport = p.split(":")[1][:4]
+    if not airport in airports:
+      final_data = {
+        "id": idx,
+        "icao": airport,
+        "positions": find_positions(positions, airport),
+        "atis_frequency": find_atis(positions, airport)
+      }
+      idx += 1
+      output.append(final_data)
+      airports.append(airport)
+  return output
+
+def find_atis(positions, icao):
+  atis = ""
+  for p in positions:
+    if p.split(":")[1][-4:] == "ATIS" and p.split(":")[1][:4] == icao:
+      atis = p.split(":")[2]
+  return atis
+
+
+def find_positions(positions, icao):
+  all_positions = []
+  for p in positions:
+    pos_data = p.split(":")
+    if "\n" in pos_data[4]:
+      identifier = pos_data[4]
+      identifier = identifier.replace("\n", "")
+    else:
+      identifier = pos_data[4]
+    
+    if not identifier[-4:] == "ATIS" and identifier[:4] == icao:
+      callsign = pos_data[1].replace(f"{identifier} ", "")
+      pos_data = {
+        "code": identifier,
+        "frequency": pos_data[2],
+        "callsign": callsign,
+        "rank":position_ranks[identifier[-3:]]['full'],
+        "solo_rank":position_ranks[identifier[-3:]]['solo']
+      }
+      all_positions.append(pos_data)
+  return all_positions
+
+output = data_builder(pfile)
+print(output)
+with open("airports.json", "w") as file:
+  output = json.dumps(output)
+  print(output, file=file)
+  file.close()
---
+++ new_versions/loginprofiles.py
@@ -0,0 +1,56 @@
+import json
+
+codes = {
+  'DEL': 2,
+  'GND': 3,
+  'TWR': 4,
+  'APP': 5,
+  'DEP': 5,
+  'CTR': 6,
+  'FSS': 1,
+}
+
+draw_distance = {
+  'DEL': 10,
+  'GND': 10,
+  'TWR': 30,
+  'APP': 120,
+  'DEP': 120,
+  'CTR': 300,
+  'FSS': 1500,
+}
+
+def organiser():
+  final_stuff = []
+  with open("VoiceChannels.txt", "r") as file:
+    positions = file.readlines()
+    file.close()
+  
+  with open("logins.txt", "w") as final:
+    final.write("PROFILE\n")
+  
+    for l in positions:
+      data = l.split(":")
+      if "\n" in data[4]:
+        identifier = data[4]
+        identifier = identifier.replace("\n", "")
+      else:
+        identifier = data[4]
+      
+      if identifier[-3:] == "CTR" or identifier[-3:] == "FSS":
+        atis_line = "Check the charts! www.chartfox.org"
+      else:
+        atis_line = f"Check the charts! chartfox.org/{identifier[:4]}"
+
+      if not identifier[-4:] == "ATIS":
+        data = f"""PROFILE:{identifier}:{draw_distance[identifier[-3:]]}:{codes[identifier[-3:]]}
+ATIS2:{data[1].replace(f'{identifier} ', '')} - EN/FR
+ATIS3:{atis_line}
+ATIS4:Leave me some feedback! vatsim.fr/fb/VOTRE_CID_ICI"""
+        final.write(f"{data}\n")
+  
+  final.close()
+
+
+
+organiser()
